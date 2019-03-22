bf5f12a2a2b9f0b38ebb7efceb970e39feadea4d
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-22 13:56:26 +0000

    adds fourth function passing (get pets sold)

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,5 +1,5 @@
 def pet_shop_name(shop)
-return shop[:name]
+  return shop[:name]
 end
 
 def total_cash(shop)
@@ -8,4 +8,8 @@ end
 
 def add_or_remove_cash(shop, amount)
   shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
 end
