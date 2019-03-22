2a1a4ed4877438c2c75da6b1aa4e3e4088637f33
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-22 14:54:47 +0000

    adds eightfh function working (find pet by name)

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,26 +2,32 @@ def pet_shop_name(shop)
   return shop[:name]
 end
 
+#2
 def total_cash(shop)
   return shop[:admin][:total_cash]
 end
 
+#3
 def add_or_remove_cash(shop, amount)
   shop[:admin][:total_cash] += amount
 end
 
+#4
 def pets_sold(shop)
   shop[:admin][:pets_sold]
 end
 
+#5
 def increase_pets_sold(shop, sold)
   shop[:admin][:pets_sold] += sold
 end
 
+#6
 def stock_count(shop)
   shop[:pets].length()
 end
 
+#7
 def pets_by_breed(shop, breed)
   pet_type = [] # Create empty array
   for type in shop[:pets] # Loop through the hashes of pets
@@ -30,4 +36,14 @@ def pets_by_breed(shop, breed)
   end
   end
   return pet_type # return the now not-empty array, which will be counted during the test
+end
+
+#8
+def find_pet_by_name(shop, name)
+  for pet in shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
 end
