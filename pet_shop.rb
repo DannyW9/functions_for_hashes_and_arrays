61cf145b9b7d3ad0b6f742b84c609d065fe85598
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-22 14:44:09 +0000

    adds seventh function (count pets by breed)

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,13 @@ end
 def stock_count(shop)
   shop[:pets].length()
 end
+
+def pets_by_breed(shop, breed)
+  pet_type = [] # Create empty array
+  for type in shop[:pets] # Loop through the hashes of pets
+   if type[:breed] == breed # Search eash hash for the key :breed that has the value breed
+    pet_type.push(type) # push each hash into the empty array if it meets the criteria
+  end
+  end
+  return pet_type # return the now not-empty array, which will be counted during the test
+end
