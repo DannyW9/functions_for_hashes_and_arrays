0d3e0b93f0d27e35c96dee4ca96643a7211415f9
Danny Welsh <dannywelsh400@hotmail.com>
2019-03-22 16:37:05 +0000

    adds second optional fuctiong passing (selling pet to customer when pet is found, when pet isn't found and when customer has insufficient funds)

--- pet_shop.rb
+++ pet_shop.rb
@@ -93,3 +93,15 @@ def customer_can_afford_pet(customer, new_pet)
     return true
   end
 end
+
+# 2
+def sell_pet_to_customer(shop, pet, customer)
+  if pet != nil
+    if customer_can_afford_pet(customer, pet) == true
+      add_pet_to_customer(customer, pet)
+      increase_pets_sold(shop, 1)
+      remove_customer_cash(customer, pet[:price])
+      add_or_remove_cash(shop, pet[:price])
+    end
+  end
+end
